995fe569b4a6a64675ac46cca993937c9f0d003b
indramukti <96394744+indramukti@users.noreply.github.com>
2022-10-30 22:15:53 +0700

    index

---
+++ src/main/java/project/pasar_memet/PasarMemetApplication.java
@@ -0,0 +1,13 @@
+package project.pasar_memet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasarMemetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasarMemetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/pasar_memet/PasarMemetApplicationTests.java
@@ -0,0 +1,13 @@
+package project.pasar_memet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasarMemetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
